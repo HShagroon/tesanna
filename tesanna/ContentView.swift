59832aac93b0e456a65f8caa7ecbf9f8785b21d3
HShagroon <haneenshagroon@gmail.com>
2023-11-23 09:25:08 +0300

    add logo image

--- tesanna/ContentView.swift
+++ tesanna/ContentView.swift
@@ -4,21 +4,47 @@
 //
 //  Created by Haneen Rida Shagroon on 02/05/1445 AH.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isActive: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 10) {
+                NavigationLink(destination: SecondView(), isActive: $isActive) {
+                    EmptyView()
+                }
+                .hidden()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        // Set isActive to true after 3 seconds to trigger the NavigationLink
+                        isActive = true
+                    }
+                }
+
+                Image("logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .edgesIgnoringSafeArea(.all)
+                                       
+            }
+            
         }
-        .padding()
+    
     }
 }
 
-#Preview {
-    ContentView()
+
+    var body: some View {
+        Text("This is the Second View")
+            .navigationTitle("Second View")
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
