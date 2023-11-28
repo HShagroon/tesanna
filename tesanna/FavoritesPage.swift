b2f7857cb6bda354fd2f3d9f9ee86497ed8f4836
Farah Alamri <ms.falamri42@gmail.com>
2023-11-28 10:21:26 +0300

    added function for displaying a view with recipe info in favorite page

--- tesanna/FavoritesPage.swift
+++ tesanna/FavoritesPage.swift
@@ -9,43 +9,45 @@ import SwiftUI
 
 struct FavoritesPage: View {
     var body: some View {
-        ScrollView{
-            ZStack{
-                
-                RoundedRectangle(cornerRadius: 8).fill(Color(.systemBackground))
-                Text(Image(systemName: "heart.fill")).font(.headline)
-                    .foregroundColor(Color("PrimaryYellow")).position(CGPoint(x: 25.0, y: 22.0))
-                
-                HStack {
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("بيتزا مارجريتا")
-                            .font(.headline)
-                        Text("التصنيف: إيطالي")
-                            .font(.caption)
-                            .padding(.top, -5.0)
-                    }.padding()
-                    Image("MargaritaPizza")
-                        .resizable()
-                        .frame(width: 152.0, height: 105.0).clipShape(UnevenRoundedRectangle(cornerRadii: .init(
-                            topLeading: 0.0,
-                            bottomLeading: 0.0,
-                            bottomTrailing: 8.0,
-                            topTrailing: 8.0
-                        ), style: .continuous))
-                    
-                    
-                        
-                    
-                        
-                }
-
-            }
+        List{
+            //get details from recipe object
+            createRecipeView(recipeName: "بيتزا مارقريتا", recipeCuisine: "إيطالي", recipeImage: "MargaritaPizza")
             
         }.padding().background(Color("background")).navigationTitle(Text("المفضلة"))
         
     }
 }
+
+//------------------------
+//functions
+func createRecipeView(recipeName: String, recipeCuisine: String, recipeImage: String) -> some View{
+    return ZStack{
+        
+        RoundedRectangle(cornerRadius: 8).fill(Color(.systemBackground))
+        Text(Image(systemName: "heart.fill")).font(.headline)
+            .foregroundColor(Color("PrimaryYellow")).position(CGPoint(x: 25.0, y: 22.0))
+        
+        HStack {
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("\(recipeName)")
+                    .font(.headline)
+                Text("التصنيف: \(recipeCuisine)")
+                    .font(.caption)
+                    .padding(.top, -5.0)
+            }.padding()
+            Image("\(recipeImage)")
+                .resizable()
+                .frame(width: 152.0, height: 105.0).clipShape(UnevenRoundedRectangle(cornerRadii: .init(
+                    topLeading: 0.0,
+                    bottomLeading: 0.0,
+                    bottomTrailing: 8.0,
+                    topTrailing: 8.0
+                ), style: .continuous))
+        }//end of HStack
+        
+    }//end of ZStack
+}//end of createRecipeView
 
 #Preview {
     FavoritesPage()
